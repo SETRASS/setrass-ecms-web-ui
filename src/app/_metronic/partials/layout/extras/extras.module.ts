3425ee676ffa9879b07e92fd167527df030ea2fe
Emanuel Sosa <emanuel_sosa@wvi.org>
2022-04-29 17:40:23 +0000

    Feature/emanuel sosa/sdt 107 crear frontend calculo prestaciones

--- src/app/_metronic/partials/layout/extras/extras.module.ts
+++ src/app/_metronic/partials/layout/extras/extras.module.ts
@@ -4,7 +4,6 @@ import {RouterModule} from '@angular/router';
 import {InlineSVGModule} from 'ng-inline-svg-2';
 import {NotificationsInnerComponent} from './dropdown-inner/notifications-inner/notifications-inner.component';
 import {QuickLinksInnerComponent} from './dropdown-inner/quick-links-inner/quick-links-inner.component';
-import {UserInnerComponent} from './dropdown-inner/user-inner/user-inner.component';
 import {LayoutScrollTopComponent} from './scroll-top/scroll-top.component';
 import {TranslationModule} from '../../../../modules/i18n';
 import {SearchResultInnerComponent} from "./dropdown-inner/search-result-inner/search-result-inner.component";
@@ -16,7 +15,6 @@ import {FormsModule} from "@angular/forms";
     NotificationsInnerComponent,
     QuickLinksInnerComponent,
     SearchResultInnerComponent,
-    UserInnerComponent,
     LayoutScrollTopComponent,
   ],
   imports: [CommonModule, FormsModule, InlineSVGModule, RouterModule, TranslationModule, NgbTooltipModule],
@@ -24,7 +22,6 @@ import {FormsModule} from "@angular/forms";
     NotificationsInnerComponent,
     QuickLinksInnerComponent,
     SearchResultInnerComponent,
-    UserInnerComponent,
     LayoutScrollTopComponent,
   ],
 })
