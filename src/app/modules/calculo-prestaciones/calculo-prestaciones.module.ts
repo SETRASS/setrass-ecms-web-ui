5cae8c1d2d6e166c8644d0c98c10316834335bdb
GersonGGuillen <gerson.guillen.fb@gmail.com>
2022-05-31 14:28:42 -0600

    [SDT-89]-Base pantalla de registro de datos del trabajador

---
+++ src/app/modules/calculo-prestaciones/calculo-prestaciones.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DatosTrabajadorComponent } from './components/datos-trabajador/datos-trabajador.component';
+import {WidgetsModule} from "../../_metronic/partials";
+
+
+
+
+@NgModule({
+  declarations: [
+    DatosTrabajadorComponent
+  ],
+  imports: [
+    CommonModule,
+    WidgetsModule
+  ]
+})
+export class CalculoPrestacionesModule { }
