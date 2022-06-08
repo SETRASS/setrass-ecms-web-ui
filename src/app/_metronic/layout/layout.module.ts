eb52ec666ef1263908ca124142d425a3ebaf5aaa
Jorge Aguilar <jorge_aguilar@wvi.org>
2022-06-08 08:43:18 -0600

    [SDT-155] Module Configure updated

--- src/app/_metronic/layout/layout.module.ts
+++ src/app/_metronic/layout/layout.module.ts
@@ -59,6 +59,6 @@ const routes: Routes = [
     NgbTooltipModule,
     TranslateModule,
   ],
-  exports: [RouterModule],
+  exports: [RouterModule, HeaderMenuComponent, LayoutComponent],
 })
 export class LayoutModule {}
