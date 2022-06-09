654832691670c55012984b2833d628e076fca699
GersonGGuillen <gerson.guillen.fb@gmail.com>
2022-06-08 23:44:00 -0600

    SDT-89_Maquetacion componente datos del trabajador

--- src/app/modules/calculo-prestaciones/components/datos-trabajador/datos-trabajador.component.ts
+++ src/app/modules/calculo-prestaciones/components/datos-trabajador/datos-trabajador.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { StepperComponent } from 'src/app/_metronic/kt/components';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as bootstrap from 'bootstrap';
+import { IStepperOptions, StepperComponent, ToggleComponent } from 'src/app/_metronic/kt/components';
 
 @Component({
   selector: 'app-datos-trabajador',
@@ -9,7 +10,28 @@ import { StepperComponent } from 'src/app/_metronic/kt/components';
 
 export class DatosTrabajadorComponent implements OnInit {
 
+  @ViewChild('kt_stepper_vertical') stepper: ElementRef;
+
+  public currentStep : Number = 1;
+  private stepperOptions: IStepperOptions = {
+    startIndex: 1,
+    animation: false,
+    animationSpeed: '',
+    animationNextClass: '',
+    animationPreviousClass: ''
+  };
+  salaryOptions:String[] = ['SI', 'NO'];
+  haveSalary: string = 'SI';
+
   constructor() {}
+
+  ngAfterViewInit(): void{
+
+    const stepper = new StepperComponent(this.stepper.nativeElement, this.stepperOptions);
+
+    stepper.on("kt.stepper.next", () => stepper.goNext());
+    stepper.on("kt.stepper.previous", () => stepper.goPrev());
+  }
 
   ngOnInit(): void {
   }
