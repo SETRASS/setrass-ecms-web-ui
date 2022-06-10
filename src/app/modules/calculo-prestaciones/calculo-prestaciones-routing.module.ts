5db1cc46e96c2b8a9e45d76f62dc165bf8c2951d
Jorge Aguilar <jorge_aguilar@wvi.org>
2022-06-10 01:41:20 -0600

    [SDT-184] Derechos e indemnizaciones component added

--- src/app/modules/calculo-prestaciones/calculo-prestaciones-routing.module.ts
+++ src/app/modules/calculo-prestaciones/calculo-prestaciones-routing.module.ts
@@ -3,14 +3,15 @@ import {NgModule} from "@angular/core";
 import {DatosEmpleadorComponent} from "./components/datos-empleador/datos-empleador.component";
 import {DatosTrabajadorComponent} from "./components/datos-trabajador/datos-trabajador.component";
 import {CalculoPrestacionesComponent} from "./pages/calculo-prestaciones/calculo-prestaciones.component";
-
+import { DerechosIdemnizacionesComponent } from "./components/derechos-idemnizaciones/derechos-idemnizaciones.component";
 const routes: Routes = [
   {
     path: '',
     children: [
       { path: '', component: CalculoPrestacionesComponent },
       { path: 'empleador', component: DatosEmpleadorComponent },
-      { path: 'trabajador', component: DatosTrabajadorComponent }
+      { path: 'trabajador', component: DatosTrabajadorComponent },
+      { path: 'derechos-indemnizaciones', component: DerechosIdemnizacionesComponent }
     ]
   }
 ];
---
+++ src/app/modules/calculo-prestaciones/components/derechos-idemnizaciones/derechos-idemnizaciones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-derechos-idemnizaciones',
+  templateUrl: './derechos-idemnizaciones.component.html',
+  styleUrls: ['./derechos-idemnizaciones.component.scss']
+})
+export class DerechosIdemnizacionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
