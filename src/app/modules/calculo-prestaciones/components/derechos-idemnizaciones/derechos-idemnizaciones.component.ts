d80f2205919e7a2d5e393bf83e2cf4a7a5acdb6c
Jorge Aguilar <jorge_aguilar@wvi.org>
2022-08-09 11:22:33 -0600

    [SDT-184] loader added

--- src/app/modules/calculo-prestaciones/components/derechos-idemnizaciones/derechos-idemnizaciones.component.ts
+++ src/app/modules/calculo-prestaciones/components/derechos-idemnizaciones/derechos-idemnizaciones.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { TerminationContractType } from 'src/app/models/enums/termination-contract-type.enum';
 import { ToolbarService } from 'src/app/_metronic/layout/components/toolbar/toolbar.service';
+
 
 
 @Component({
@@ -10,15 +11,24 @@ import { ToolbarService } from 'src/app/_metronic/layout/components/toolbar/tool
 })
 export class DerechosIdemnizacionesComponent implements OnInit {
 
+  @ViewChild('overlay') $overlay: ElementRef;
+
   currentContractType: TerminationContractType;
 
-  constructor(public contractType: ToolbarService) {
+  constructor(public contractType: ToolbarService, 
+    private render2: Renderer2) {
 
    }
 
   ngOnInit(): void {
     this.currentContractType = this.contractType.terminationContractType;
     console.log(this.currentContractType);
+  }
+
+  recalculo(){
+    this.render2.addClass(this.$overlay.nativeElement, 'active-overlay');
+    setTimeout(() => this.render2.removeClass(this.$overlay.nativeElement, 'active-overlay'), 8000);
+    
   }
 
 }
