3edf67c510dfda6e084bdfecb8c05b331c3bb18e
Jorge Aguilar <jorge_aguilar@wvi.org>
2023-03-28 16:25:57 -0600

    staff module added

---
+++ src/app/modules/staff/staff.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ecms-staff',
+  template: '<lib-sihcal-staff></lib-sihcal-staff>',
+  styleUrls: ['./staff.component.scss']
+})
+export class StaffComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/routing.ts
+++ src/app/pages/routing.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { CalculoPrestacionesModule } from '../modules/calculo-prestaciones/calculo-prestaciones.module';
 import { PatronoEmpleadorModule } from '../modules/patrono-empleador/patrono-empleador.module';
+import { StaffModule } from '../modules/staff/staff.module';
 
 const Routing: Routes = [
+  {
+    path: 'staff',
+    loadChildren: () => StaffModule
+  },
   {
     path: 'patrono-empleador',
     loadChildren: () => PatronoEmpleadorModule
